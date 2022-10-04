d35ba9aaaf16272fd29d498ecfbfad3752de5629
wannjb <113450108+wannjb@users.noreply.github.com>
2022-10-04 17:47:24 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -41,23 +41,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['6285850539404'] //ur owner number
-global.ownername = "6285850539404"
-global.ytname = "YT:https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ" //ur yt chanel name
-global.socialm = "GitHub: Jarot" //ur github or insta name
+global.owner = ['62895327999292'] //ur owner number
+global.ownername = "WannTrue"
+global.ytname = "https://www.instagram.com/mhdkrnwnn_" //ur yt chanel name
+global.socialm = "GitHub: wannjb" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bombdy
-global.botname = "𝐑𝐢𝐬𝐦𝐚-ᴍᴅ᭄" //ur bot name
-global.websitex = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ" //ur website
+global.botname = "WannTrue" //ur bot name
+global.websitex = "https://www.instagram.com/mhdkrnwnn_" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/eFNTvNt.mp4' } //gif and video menu
-global.packname = "𝐑𝐢𝐬𝐦𝐚-ᴍᴅ᭄ Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "𝐑𝐢𝐬𝐦𝐚-ᴍᴅ᭄"
+global.packname = "WannTrue Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
+global.author = "WannTrue"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['6285850539404'] //ur owner tag
-global.ownernummenu = ['6285850539404'] //ur owner number in menu and all
-global.watermark = "𝐑𝐢𝐬𝐦𝐚-ᴍᴅ᭄." //ur watermark
+global.ownertag = ['62895327999292'] //ur owner tag
+global.ownernummenu = ['62895327999292'] //ur owner number in menu and all
+global.watermark = "WannTrue᭄." //ur watermark
 global.botscript = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ"
 global.linkz1 = ""
 global.linkz2 = ""
